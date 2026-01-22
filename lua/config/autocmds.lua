9d9d89ab70b15f55a3346e9095b1b7c5d17e116a
dwat3r <ohechtl@elmar.nl>
2026-01-22 12:22:13 +0100

    make NextLint async

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,23 +7,54 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_create_user_command("NextLint", function()
-  local output = vim.fn.system("pnpm next lint --format compact")
-  local lines = vim.split(output, "\n")
-  local qf_list = {}
+  print("Running next lint...")
 
-  for _, line in ipairs(lines) do
-    -- Parse next lint output format
-    local file, lnum, col, text = line:match("(.+): line ([0-9]+), col ([0-9]+), (.+)")
-    if file then
-      table.insert(qf_list, {
-        filename = file,
-        lnum = tonumber(lnum),
-        col = tonumber(col),
-        text = text,
-      })
-    end
-  end
+  local output_lines = {}
 
-  vim.fn.setqflist(qf_list)
-  vim.cmd("copen")
+  vim.fn.jobstart("pnpm next lint --format compact", {
+    stdout_buffered = true,
+    stderr_buffered = true,
+
+    on_stdout = function(_, data)
+      if data then
+        vim.list_extend(output_lines, data)
+      end
+    end,
+
+    on_stderr = function(_, data)
+      if data then
+        vim.list_extend(output_lines, data)
+      end
+    end,
+
+    on_exit = function(_, exit_code)
+      vim.schedule(function()
+        local qf_list = {}
+
+        for _, line in ipairs(output_lines) do
+          if line ~= "" then
+            -- Parse next lint output format
+            local file, lnum, col, text = line:match("(.+): line ([0-9]+), col ([0-9]+), (.+)")
+            if file then
+              table.insert(qf_list, {
+                filename = file,
+                lnum = tonumber(lnum),
+                col = tonumber(col),
+                text = text,
+              })
+            end
+          end
+        end
+
+        vim.fn.setqflist(qf_list)
+        vim.cmd("copen")
+
+        if #qf_list > 0 then
+          print(string.format("Found %d lint issues", #qf_list))
+        else
+          print("No lint issues found")
+        end
+      end)
+    end,
+  })
 end, {})
